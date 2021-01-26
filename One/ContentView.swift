698b2837df29978c4609a39b53f097dd9b61d1fb
Felipe Vallejo <stephanovallejom@gmail.com>
2021-01-26 13:23:31 -0500

    Initial Commit

---
+++ One/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  One
+//
+//  Created by Felipe Vallejo on 1/26/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
